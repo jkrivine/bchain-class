695d67bf553ed437033eb802e3921e9d95226dca
jkrivine <jean@mangrove.exchange>
2024-01-21 16:49:56 +0100

    Ballot without reentrancy

--- src/Ballot.sol
+++ src/Ballot.sol
@@ -6,19 +6,19 @@ pragma solidity ^0.8.13;
 
 contract Ballot {
     enum Vote {
-      ABS, // Abstain
-      YES, // YES
-      NO // NO
+        ABS, // Abstain
+        YES, // YES
+        NO // NO
     }
 
     ///@notice Mapping of votes to their count.
-    mapping (Vote vote => uint count) public votes;
+    mapping(Vote vote => uint256 count) public votes;
 
     ///@notice List of addresses that are still allowed to vote.
     address[] public whitelist;
 
     ///@notice Amount of wei to refund to each voter.
-    uint public refundPerVote;
+    uint256 public refundPerVote;
 
     ///@notice Address of the admin of the contract.
     address public admin;
@@ -27,86 +27,86 @@ contract Ballot {
     event SetAdmin(address);
 
     ///@notice Event emitted when a vote is opened.
-    event OpenVote(uint bounty, address[] whitelist);
+    event OpenVote(uint256 bounty, address[] whitelist);
 
     ///@notice Event emitted when a vote is cast.
-    event LogVote(address voter, uint voterId, Vote t);
+    event LogVote(address voter, uint256 voterId, Vote t);
 
     ///@notice Event emitted when a vote is closed.
     event CloseVote(Vote winningVote);
 
     ///@notice Constructor that sets the admin to the deployer.
-    constructor () {
-      admin = msg.sender;
+    constructor() {
+        admin = msg.sender;
     }
 
     ///@notice Function to get the number of participants.
-    function participants() external view returns (uint) {
-      return whitelist.length;
+    function participants() external view returns (uint256) {
+        return whitelist.length;
     }
 
     ///@notice Function to change the admin of the contract.
     ///@param admin_ Address of the new admin.
     function setAdmin(address admin_) external {
-      require(admin_ != admin && admin != address(0), "Ballot/InvalidAdmin");
-      admin = admin_;
-      emit SetAdmin(admin_);
+        require(admin_ != admin && admin != address(0), "Ballot/InvalidAdmin");
+        admin = admin_;
+        emit SetAdmin(admin_);
     }
 
     ///@notice Function to open a vote.
     ///@param bounty Bounty to be paid to each voter.
     ///@param whitelist_ List of addresses that are allowed to vote.
-    function open(uint bounty, address[] calldata whitelist_) external payable {
-      require(msg.sender == admin, "Ballot/Unauthorized");
-      require(bounty * whitelist_.length <= msg.value, "Ballot/NotEnoughFunds");
-      refundPerVote = bounty;
-      whitelist = new address[](whitelist_.length);
-      for (uint i; i<whitelist_.length; i++) {
-        whitelist[i] = whitelist_[i];
-      }
-      emit OpenVote(bounty, whitelist_);
+    function open(uint256 bounty, address[] calldata whitelist_) external payable {
+        require(msg.sender == admin, "Ballot/Unauthorized");
+        require(bounty * whitelist_.length <= msg.value, "Ballot/NotEnoughFunds");
+        refundPerVote = bounty;
+        whitelist = new address[](whitelist_.length);
+        for (uint256 i; i < whitelist_.length; i++) {
+            whitelist[i] = whitelist_[i];
+        }
+        emit OpenVote(bounty, whitelist_);
     }
 
     ///@notice Function to get the id of a voter.
     ///@param voter Address of the voter.
     ///@return id Id of the voter.
-    function getId(address voter) external view returns (uint id) {
-      for(uint i; i< whitelist.length; i++){
-        if(whitelist[i]==voter) {
-          return i;
+    function getId(address voter) external view returns (uint256 id) {
+        for (uint256 i; i < whitelist.length; i++) {
+            if (whitelist[i] == voter) {
+                return i;
+            }
         }
-      }
-      require(false, "Ballot/NotFound");
+        require(false, "Ballot/NotFound");
     }
 
     ///@notice Function to close a vote.
     ///@return winningVote Vote that won. Vote ABS if there was a tie.
     ///@dev Refunds the admin for their gas processing costs.
     function closeVote() external returns (Vote winningVote) {
-      require(msg.sender == admin, "Ballot/Unauthorized");
-      (bool success,) = admin.call{value: address(this).balance}("");
-      require(success, "Ballot/FailedToWithdraw");
-      whitelist = new address[](0);
-      winningVote = votes[Vote.YES] > votes[Vote.NO] ? Vote.YES : votes[Vote.YES] < votes[Vote.NO] ? Vote.NO : Vote.ABS;
-      // reset votes mapping
-      votes[Vote.YES] = 0;
-      votes[Vote.NO] = 0;
-      votes[Vote.ABS] = 0;
-      refundPerVote = 0;
-      emit CloseVote(winningVote);
+        require(msg.sender == admin, "Ballot/Unauthorized");
+        (bool success,) = admin.call{value: address(this).balance}("");
+        require(success, "Ballot/FailedToWithdraw");
+        whitelist = new address[](0);
+        winningVote =
+            votes[Vote.YES] > votes[Vote.NO] ? Vote.YES : votes[Vote.YES] < votes[Vote.NO] ? Vote.NO : Vote.ABS;
+        // reset votes mapping
+        votes[Vote.YES] = 0;
+        votes[Vote.NO] = 0;
+        votes[Vote.ABS] = 0;
+        refundPerVote = 0;
+        emit CloseVote(winningVote);
     }
 
     ///@notice Function to vote.
     ///@param voterId Id of the voter.
     ///@param t Vote of the voter.
     ///@dev Refunds the voter for their gas processing costs according to the `refundPerVote` parameter.
-    function vote(uint voterId, Vote t) external {
-      require(whitelist[voterId] == msg.sender, "Ballot/Unauthorized");
-      (bool success,) = msg.sender.call{value: refundPerVote}("");
-      require(success, "Ballot/CouldNotRefundVoter");
-      votes[t]++;
-      whitelist[voterId] = address(0);
-      emit LogVote(msg.sender, voterId, t);
+    function vote(uint256 voterId, Vote t) external {
+        require(whitelist[voterId] == msg.sender, "Ballot/Unauthorized");
+        votes[t]++;
+        whitelist[voterId] = address(0);
+        emit LogVote(msg.sender, voterId, t);
+        (bool success,) = msg.sender.call{value: refundPerVote}("");
+        require(success, "Ballot/CouldNotRefundVoter");
     }
-
 }
--- test/Ballot.t.sol
+++ test/Ballot.t.sol
@@ -6,11 +6,22 @@ import {Ballot} from "../src/Ballot.sol";
 
 contract BallotTest is Test {
     Ballot public ballot;
-    event OpenVote(uint bounty, address[] whitelist);
-    event LogVote(address voter, uint voterId, Ballot.Vote t);
+
+    event OpenVote(uint256 bounty, address[] whitelist);
+    event LogVote(address voter, uint256 voterId, Ballot.Vote t);
     event CloseVote(Ballot.Vote winningVote);
 
-    receive() external virtual payable {}
+    bool internal reenter;
+    uint256 internal attacker_id;
+
+    receive() external payable {
+        // reentrancy attack after casting a vote
+        // one tries to cast again a vote during the refund
+        if (reenter) {
+            vm.expectRevert("Ballot/Unauthorized");
+            ballot.vote(attacker_id, Ballot.Vote.YES);
+        }
+    }
 
     function setUp() public {
         ballot = new Ballot();
@@ -18,11 +29,11 @@ contract BallotTest is Test {
         participants[0] = address(this);
         participants[1] = vm.addr(1);
         participants[2] = vm.addr(2);
-        
+
         vm.expectEmit(true, true, true, true);
         emit OpenVote(0.01 ether, participants);
 
-        ballot.open{value:1 ether}(0.01 ether, participants);
+        ballot.open{value: 1 ether}(0.01 ether, participants);
     }
 
     function test_vote() public {
@@ -34,21 +45,29 @@ contract BallotTest is Test {
         emit LogVote(address(this), 0, Ballot.Vote.YES);
         ballot.vote(0, Ballot.Vote.YES);
 
-        uint bal_before = voter1.balance;
+        uint256 bal_before = voter1.balance;
         vm.prank(voter1);
         ballot.vote(1, Ballot.Vote.ABS);
         assertEq(voter1.balance, bal_before + 0.01 ether);
-                
+
         vm.prank(voter2);
-        ballot.vote(2,Ballot.Vote.NO);
-        
+        ballot.vote(2, Ballot.Vote.NO);
+
         assertEq(ballot.votes(Ballot.Vote.YES), 1);
-        assertTrue(ballot.whitelist(0)==address(0));
+        assertTrue(ballot.whitelist(0) == address(0));
 
         vm.expectEmit(true, true, true, true);
         emit CloseVote(Ballot.Vote.ABS);
 
         Ballot.Vote winningVote = ballot.closeVote();
-        assertEq(uint(winningVote), uint(Ballot.Vote.ABS));
+        assertEq(uint256(winningVote), uint256(Ballot.Vote.ABS));
+    }
+
+    function test_no_reentrancy() public {
+        reenter = true;
+        attacker_id = 0;
+        // reentrant call attempt should make the refund fail
+        vm.expectRevert("Ballot/CouldNotRefundVoter");
+        ballot.vote(0, Ballot.Vote.YES);
     }
 }
